4e84d6283fbaac97b8297d82cd249a91052c345d
GowthamanT <65595132+GowthamanT@users.noreply.github.com>
2020-10-28 22:14:19 +0530

    Update ThemeParkRideController.java

--- src/main/java/com/tg/controller/ThemeParkRideController.java
+++ src/main/java/com/tg/controller/ThemeParkRideController.java
@@ -27,16 +27,4 @@ public class ThemeParkRideController {
         return themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
     }
     
-    @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide themeParkRide) {
-        return themeParkRideRepository.save(themeParkRide);
-    }
-
-    @PostMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ThemeParkRide postRide(@PathVariable long id){
-        return themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
-    }
-
 }
-
-//consumes = MediaType.APPLICATION_JSON_VALUE,
